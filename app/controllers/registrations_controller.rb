ad691093453a9f2e8da3e7a4b6bbe8351b21e1e2
tkdtktd <tkdtktd3104@gmail.com>
2018-09-06 03:55:27 +0900

    Finish user index action

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -38,4 +38,9 @@ def after_sign_up_path_for(resource)
       def sign_up_params
         params.require(:user).permit(:name, :email, :password, :password_confirmation)
       end
+
+      def account_update_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation, :current_password)
+      end
+
 end
