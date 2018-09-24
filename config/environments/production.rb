1e2191adf03fa635b381bd3903ee8866037b6c36
tkdtktd <tkdtktd3104@gmail.com>
2018-09-25 03:42:53 +0900

    Correct action mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'https://samuraisampleapp.herokuapp.com/' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    # port: 587,
+    port: 465,
+    domain: "gmail.com",
+    authentication: "login",
+    enable_starttls_auto: true,
+    user_name: ENV['SMTP_MAIL'],
+    password: ENV['SMTP_PASS'],
+    tls: true,
+  }
 end
